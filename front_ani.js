5e087294a8d3543b2956c68ff5181d633d4247a3
esthertch <esther.tch@hotmail.com>
2020-08-04 18:14:50 +0800

    Create front_ani.js

---
+++ front_ani.js
@@ -0,0 +1,93 @@
+/** frontpage animation **/
+
+var x = document.querySelectorAll('#one');
+var i = 0;
+
+while(i < x.length){
+    x[i].onmouseover = function change(){
+        var i = 0;
+        while(i < x.length){
+            x[i].style.color = '#B2584D';
+            i++;
+        }
+    }
+
+    x[i].onmouseout = function change(){
+        var i = 0;
+        while(i < x.length){
+            x[i].style.color = '';
+            i++;
+        }
+    }
+
+    i++;
+}
+
+var z = document.querySelectorAll('#two');
+var i = 0;
+
+while(i < z.length){
+    z[i].onmouseover = function change(){
+        var i = 0;
+        while(i < z.length){
+            z[i].style.color = '#B2584D';
+            i++;
+        }
+    }
+
+    z[i].onmouseout = function change(){
+        var i = 0;
+        while(i < z.length){
+            z[i].style.color = '';
+            i++;
+        }
+    }
+
+    i++;
+}
+
+var y = document.querySelectorAll('#three');
+var i = 0;
+
+while(i < y.length){
+    y[i].onmouseover = function change(){
+        var i = 0;
+        while(i <y.length){
+            y[i].style.color = '#B2584D';
+            i++;
+        }
+    }
+
+    y[i].onmouseout = function change(){
+        var i = 0;
+        while(i < y.length){
+           y[i].style.color = '';
+            i++;
+        }
+    }
+
+    i++;
+}
+
+var w = document.querySelectorAll('#four');
+var i = 0;
+
+while(i < w.length){
+    w[i].onmouseover = function change(){
+        var i = 0;
+        while(i <w.length){
+            w[i].style.color = '#B2584D';
+            i++;
+        }
+    }
+
+    w[i].onmouseout = function change(){
+        var i = 0;
+        while(i < w.length){
+           w[i].style.color = '';
+            i++;
+        }
+    }
+
+    i++;
+}
